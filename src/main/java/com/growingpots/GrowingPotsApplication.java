3bac8fdfb0b6fe3192b86d3a6456f60db2453790
이용민 <bacaren21@naver.com>
2026-06-22 16:54:50 +0900

    init

---
+++ src/main/java/com/growingpots/GrowingPotsApplication.java
@@ -0,0 +1,13 @@
+package com.growingpots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowingPotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowingPotsApplication.class, args);
+	}
+
+}
