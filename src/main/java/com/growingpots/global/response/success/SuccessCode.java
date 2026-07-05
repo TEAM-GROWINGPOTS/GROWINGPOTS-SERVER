b0f399bf9047ad4379f7916317e57540d416c152
5eoyng <szoyoung@naver.com>
2026-07-05 23:20:36 +0900

    chore(#20): 성공, 에러 코드 추가

--- src/main/java/com/growingpots/global/response/success/SuccessCode.java
+++ src/main/java/com/growingpots/global/response/success/SuccessCode.java
@@ -21,6 +21,7 @@ public enum SuccessCode implements SuccessType {
     // User
     USER_FOUND(HttpStatus.OK, "USER_200", "사용자 조회에 성공했습니다."),
     USER_UPDATED(HttpStatus.OK, "USER_200_1", "사용자 정보가 수정됐습니다."),
+    STUDENT_PROFILE_CREATED(HttpStatus.CREATED, "USER_201", "학적 정보가 저장되었습니다."),
 
     // University
     UNIVERSITY_LIST_FOUND(HttpStatus.OK, "UNIV_200", "학교 목록 조회에 성공했습니다."),
