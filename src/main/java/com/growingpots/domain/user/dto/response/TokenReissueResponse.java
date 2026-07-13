ee4b63477a752e667672cadaaa5cd54bc6f0b6e4
5eoyng <szoyoung@naver.com>
2026-07-13 21:46:53 +0900

    refactor(#153): TokenReissueResponse 제거

--- src/main/java/com/growingpots/domain/user/dto/response/TokenReissueResponse.java
+++
@@ -1,8 +0,0 @@
-package com.growingpots.domain.user.dto.response;
-
-import io.swagger.v3.oas.annotations.media.Schema;
-
-public record TokenReissueResponse(
-        @Schema(description = "재발급된 서비스 accessToken") String accessToken
-) {
-}
