95d1aec675c55c97f11f575d275f2688d0932c62
5eoyng <szoyoung@naver.com>
2026-07-13 23:08:17 +0900

    docs(#157): 자물쇠 제거, 스웨거 반영

--- src/main/java/com/growingpots/global/config/SwaggerConfig.java
+++ src/main/java/com/growingpots/global/config/SwaggerConfig.java
@@ -1,32 +1,19 @@
 package com.growingpots.global.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
-    private static final String COOKIE_SCHEME = "accessToken";
-
     @Bean
     public OpenAPI openAPI() {
-        SecurityScheme securityScheme = new SecurityScheme()
-                .type(SecurityScheme.Type.APIKEY)
-                .in(SecurityScheme.In.COOKIE)
-                .name(COOKIE_SCHEME);
-
         return new OpenAPI()
                 .info(new Info()
                         .title("GrowingPots API")
                         .description("GrowingPots 서버 API 문서")
-                        .version("v1"))
-                .addSecurityItem(new SecurityRequirement().addList(COOKIE_SCHEME))
-                .components(new Components()
-                        .addSecuritySchemes(COOKIE_SCHEME, securityScheme));
+                        .version("v1"));
     }
 }
